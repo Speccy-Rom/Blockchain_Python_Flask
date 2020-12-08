48c6a77e570a69e6f9c9b56c827a09588853f57c
Speccy-rom <speccy.rom@gmail.com>
2020-12-08 23:50:08 +0300

    add входные параметры

--- Flask/app/block.py
+++ Flask/app/block.py
@@ -1,18 +1,34 @@
 import json
+import os
 
 
-def write_block():
-    data = {'name': 'ivan',
-            'amount': 5,
-            'to_whom': 'vasja',
-            'hash': '1234'
+
+def get_hash(filename):
+    blockchain_dir = os.curdir + '/blockchain/'
+
+
+def write_block(name, amount, to_whom, prev_hash=' '):  # входные параметры
+    blockchain_dir = os.curdir + '/blockchain/'
+
+    files = os.listdir(blockchain_dir) # получение файлов папки
+    files = sorted([i for i in files])
+
+    last_file = files[-1]
+    filename = str(last_file + 1)
+
+    # print(filename)
+    data = {'name': name,
+            'amount': amount,
+            'to_whom': to_whom,
+            'hash': prev_hash
             }
-    with open ('test', 'w') as file:
+    with open(blockchain_dir + 'test', 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    write_block()
+    write_block(name='Roman', amount=2, to_whom='Olga')
+
 
 if __name__ == '__main__':
     main()
